ff6d63df79ff64e42f9eeb77ee1920324c19bc00
jayed969 <jayed.swe@gmail.com>
2023-03-28 06:11:49 +0600

    added Rest-Counties-API

---
+++ Rest-Counties-API/countries.js
@@ -0,0 +1,24 @@
+const loadCountries = () =>{
+    fetch("https://restcountries.com/v3.1/all")
+    .then(res=>res.json())
+    .then(data=>displayCountries(data))
+}
+
+const displayCountries = (countries) =>{
+    console.log(countries[0])
+    const countriesHTML = countries.map(country=>getCountriesHTML(country))
+    const container = document.getElementById("countries");
+    container.innerHTML = countriesHTML.join(' ');
+
+}
+
+const getCountriesHTML = (country) =>{
+    return `
+    <div class="country">
+        <h2>${country.name.common}</h2>
+        <img class="countryImage" src=${country.flags.png}>
+    </div>
+    `
+}
+
+loadCountries()
