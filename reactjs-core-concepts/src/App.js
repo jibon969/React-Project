d9c409201904b7f27ba313a90b41cdb9c766101c
jayed969 <jayed.swe@gmail.com>
2023-03-29 05:03:37 +0600

    added Create Component, return HTML from a component

--- reactjs-core-concepts/src/App.js
+++ reactjs-core-concepts/src/App.js
@@ -1,37 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
 
-const number = 6666;
-const singer = {name:"Mr. Mahfuz", job:"Singer"}
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>JSX</h1>
-        <div className="container">
-          <h3>Hello Dude. React. How are you ?</h3>
-        </div>
-        <div className="music">
-          <p>Number : {number}</p>
-          <p>Name : {singer.name} {singer.job}</p>
-        </div>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p></p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Person></Person>
+      <Person></Person>
+      <Person></Person>
+      <h5>New Component. YAY</h5>
+      <Friend></Friend>
+      <Friend></Friend>
     </div>
   );
+}
+
+function Person (){
+  return(
+    <div className="person">
+      <h1>Jibon Ahmed</h1>
+      <p>Profession : Software Engineer</p>
+    </div>
+  )
+}
+function Friend(){
+  return(
+    <div className="friend">
+      <h2>Sakib Al Hasan</h2>
+      <p>Profession : Cricket</p>
+    </div>
+  )
 }
 
 export default App;
