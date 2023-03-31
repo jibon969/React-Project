1f3f505fc97b53893b727fac9dfb8ffd8b816208
jayed969 <jayed.swe@gmail.com>
2023-03-31 11:12:31 +0600

    added React Extension Pack and component with css file

--- react-rest-countries/src/App.js
+++ react-rest-countries/src/App.js
@@ -1,40 +1,46 @@
 import './App.css';
 import {useEffect, useState} from 'react';
+import Countries from './components/Countries/Countries';
+import Person from './components/Person/Person';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
-      <LoadCountries></LoadCountries>
+      <Header></Header>
+      <Countries></Countries>
+      <Person></Person>
     </div>
   );
 }
 
-function LoadCountries(){
-  const [countries, setCountries] = useState([])
-  useEffect(()=>{
-    fetch('https://restcountries.com/v3.1/all')
-    .then(res => res.json())
-    .then(data=>setCountries(data))
-  },[])
 
-  return(
-    <div>
-      <h1>Visiting Every country of the World !!!</h1>
-      <h1>Available Countries : {countries.length}</h1>
-      {
-        countries.map(country=><Country name={country.name.common} population={country.population}></Country>)
-      }
+// function LoadCountries(){
+//   const [countries, setCountries] = useState([])
+//   useEffect(()=>{
+//     fetch('https://restcountries.com/v3.1/all')
+//     .then(res => res.json())
+//     .then(data=>setCountries(data))
+//   },[])
+
+//   return(
+//     <div>
+//       <h1>Visiting Every country of the World !!!</h1>
+//       <h1>Available Countries : {countries.length}</h1>
+//       {
+//         countries.map(country=><Country name={country.name.common} population={country.population}></Country>)
+//       }
     
-    </div>
-  )
-}
+//     </div>
+//   )
+// }
 
-function Country(props){
-  return(
-    <div>
-      <h2>{props.name}</h2>
-      <h4>{props.population}</h4>
-    </div>
-  )
-}
+// function Country(props){
+//   return(
+//     <div>
+//       <h2>{props.name}</h2>
+//       <h4>{props.population}</h4>
+//     </div>
+//   )
+// }
 export default App;
