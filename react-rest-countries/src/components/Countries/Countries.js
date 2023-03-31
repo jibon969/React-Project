28b6e80576f5deb2a8925489e5d5fdb06b16442f
jayed969 <jayed.swe@gmail.com>
2023-03-31 12:05:25 +0600

    added Create child component and pass data via props

--- react-rest-countries/src/components/Countries/Countries.js
+++ react-rest-countries/src/components/Countries/Countries.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Country from '../Country/Country';
+import './Countries.css'
 
 const Countries = () => {
+    const [countries, setCountries] = useState([])
+    useEffect( ()=>{
+        fetch('https://restcountries.com/v3.1/all')
+        .then(res=> res.json())
+        .then(data=> setCountries(data))
+
+    }, [])
+
     return (
         <div>
-            <h2>Countries in other Files.</h2>
+            <h1>Visiting Every country of the World !!!</h1>
+            <h2>Available Countries : {countries.length}</h2>
+            {
+                countries.map(country => 
+                <Country 
+                    name={country.name.common}
+                    population={country.population}
+                    area={country.area}
+                >
+                </Country>)
+            }
         </div>
     );
 };
