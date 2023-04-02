b46ffd311c81b7110baf28a9c0050986cbb1ff80
jayed969 <jayed.swe@gmail.com>
2023-04-02 22:34:56 +0600

    added destring

--- reactjs-core-concepts-recap/src/components/Task/Task.js
+++ reactjs-core-concepts-recap/src/components/Task/Task.js
@@ -2,14 +2,14 @@ import React from 'react';
 import './Task.css'
 
 const Task = (props) => {
-    // const [id, title, completed] = props.todo;
+    const {id, title, completed} = props.todo;
     // console.log(props)
     return (
         <div>
             <div className='task'>
-                <h2>{props.todo.id}</h2>
-                <h3>{props.todo.title}</h3>
-                <h4>{props.todo.completed}</h4>
+                <h2>{id}</h2>
+                <h3>{title}</h3>
+                <h4>{completed}</h4>
             </div>
         </div>
     );
