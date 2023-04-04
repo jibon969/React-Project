82fabfee83444f6f383e4bae36c4e446d9cd8406
jayed969 <jayed.swe@gmail.com>
2023-04-04 13:26:10 +0600

    added useState

--- how-react-works/src/components/Device/Device.js
+++ how-react-works/src/components/Device/Device.js
@@ -2,6 +2,7 @@ import React from 'react';
 import DeviceDetail from '../DeviceDetail/DeviceDetail';
 
 const Device = (props) => {
+    
     return (
         <div>
             <h2>I have : {props.name}</h2>
