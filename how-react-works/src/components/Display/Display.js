93a00cbb116196a3db77d5d4e0e459d54c32091f
jayed969 <jayed.swe@gmail.com>
2023-04-05 22:20:28 +0600

    added How React Hook works, send state via props , props vs state

--- how-react-works/src/components/Display/Display.js
+++ how-react-works/src/components/Display/Display.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import Dial from '../Dial/Dial';
 
 const Display = (props) => {
     return (
         <div style={{border:"1px solid green", margin:"10px"}}>
+            <h1>Display</h1>
             <h2>So far step today : {props.steps}</h2>
+            <Dial steps={props.steps}></Dial>
         </div>
     );
 };
