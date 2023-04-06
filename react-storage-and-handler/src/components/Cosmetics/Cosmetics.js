8f4bdaab67571a5fe69eea3f271a1d238752d9bc
jayed969 <jayed.swe@gmail.com>
2023-04-06 10:34:53 +0600

    added import default, relative path

--- react-storage-and-handler/src/components/Cosmetics/Cosmetics.js
+++ react-storage-and-handler/src/components/Cosmetics/Cosmetics.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { add, multiply } from '../utilities/calculate';
+
+// import add from '../utilities/calculate';
 
 const Cosmetics = () => {
+    const first = 20;
+    const second = 10;
+    const total = add(first, second)
+    const result = multiply(first, second)
     return (
         <div>
             <h1>Welcome to my cosmetics store</h1>
+            <p>Total : {total}</p>
+            <p>Multiply : {result}</p>
         </div>
     );
 };
---
+++ react-storage-and-handler/src/components/utilities/calculate.js
@@ -0,0 +1,12 @@
+const add = (first, second) => {
+    return first + second
+};
+
+const multiply = (first, second) => {
+    return first * second
+};
+
+// Multiple function
+export {add, multiply}
+
+// export default add;
