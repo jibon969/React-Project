526bde3d45368b3e5176e9d11cd9bf508387a636
jayed969 <jayed.swe@gmail.com>
2023-04-12 14:20:28 +0600

    how to export file in react

--- react-storage-and-handler/src/components/Cosmetics/Cosmetics.js
+++ react-storage-and-handler/src/components/Cosmetics/Cosmetics.js
@@ -1,14 +1,7 @@
 import React, { useEffect, useState } from 'react';
-import { add, multiply } from '../utilities/calculate';
 import Cosmetic from '../Cosmetic/Cosmetic';
 
-// import add from '../utilities/calculate';
-
 const Cosmetics = () => {
-    const first = 20;
-    const second = 10;
-    const total = add(first, second)
-    const result = multiply(first, second)
     const [cosmetics, setCosmetics] = useState([])
     
     useEffect(()=>{
@@ -20,8 +13,6 @@ const Cosmetics = () => {
     return (
         <div>
             <h1>Welcome to my cosmetics store</h1>
-            <p>Total : {total}</p>
-            <p>Multiply : {result}</p>
             {
                 cosmetics.map(cosmetic=><Cosmetic key={cosmetic.id} cosmetic={cosmetic}></Cosmetic>)
             }
