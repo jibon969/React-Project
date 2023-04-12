4c7c589fcfce76f98cfd26a59c308ce28cea9b6a
jayed969 <jayed.swe@gmail.com>
2023-04-12 14:32:47 +0600

    added manage to localstaorage data

--- react-storage-and-handler/src/components/utilities/calculate.js
+++ react-storage-and-handler/src/components/utilities/calculate.js
@@ -3,8 +3,24 @@ const addTwoNumber = (a, b) =>{
     return a + b
 }
 
+// use local storage to manage data
 const addToDb = (id) =>{
-    console.log("Hello, there ...", id)
+    let shoppingCart;
+    const storedCart = localStorage.getItem('shopping-cart');
+    if(storedCart){
+        shoppingCart = JSON.parse(storedCart)
+    }
+    
+    const quantity = shoppingCart[id]
+
+    if(quantity){
+        const newQuantity = parseInt(quantity) + 1;
+        localStorage.setItem(id, newQuantity);
+    }
+    else{
+        shoppingCart[id] = 1;
+    }
+    localStorage.setItem('shopping-cart', JSON.stringify(shoppingCart))
 }
 
 export {addTwoNumber, addToDb}
