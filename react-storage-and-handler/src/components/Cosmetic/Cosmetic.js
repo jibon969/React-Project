de250dc72fbd4a90f6eceb0dc8b6898a149e572a
jayed969 <jayed.swe@gmail.com>
2023-04-12 14:29:50 +0600

    added remove fake.js files

--- react-storage-and-handler/src/components/Cosmetic/Cosmetic.js
+++ react-storage-and-handler/src/components/Cosmetic/Cosmetic.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import './Cosmetic.css'
-import { addTwoNumber } from '../utilities/calculate';
+import { addToDb, addTwoNumber } from '../utilities/calculate';
 
 const Cosmetic = (props) => {
     const {title, price, id} = props.cosmetic;
-   const result =  addTwoNumber(10, 30)
+    const result =  addTwoNumber(10, 30)
+
+    const addToCart = (id) =>{
+        addToDb(id)
+    }
+
     return (
         <div className='product'>
             <h3>Buy this  : {title}</h3>
             <p>Only for $ : {price}</p>
             <p>Its has id : {id}</p>
             <p>Sum : {result}</p>
-            <button>Add to Cart</button>
+            <button onClick={()=>addToCart(id)}>Add to Cart</button>
         </div>
     );
 };
