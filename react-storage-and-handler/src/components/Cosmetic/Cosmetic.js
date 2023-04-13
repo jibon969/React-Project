9fb0793cd9227c30a362814ad5284ffa99b2efac
jayed969 <jayed.swe@gmail.com>
2023-04-13 14:32:17 +0600

    added localstorage save data

--- react-storage-and-handler/src/components/Cosmetic/Cosmetic.js
+++ react-storage-and-handler/src/components/Cosmetic/Cosmetic.js
@@ -2,19 +2,17 @@ import React from 'react';
 import './Cosmetic.css'
 import { addToDb } from '../utilities/fakedb';
 
-
 const Cosmetic = (props) => {
     const {title, price, id} = props.cosmetic;
     const addToCart = (id) =>{
         addToDb(id)
     }
-
     return (
         <div className='product'>
             <h3>Buy this  : {title}</h3>
             <p>Only for $ : {price}</p>
             <p>Its has id : {id}</p>
-            <button onClick={() =>addToCart(id)}>Add to cart</button>
+            <button onClick={()=>addToCart(id)}>Add to Cart</button>
         </div>
     );
 };
