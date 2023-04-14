3092be7f7692718400cddabe12a4af439956afb4
jayed969 <jayed.swe@gmail.com>
2023-04-14 23:11:52 +0600

    added item reduce

--- react-storage-and-handler/src/components/utilities/reduce.js
+++ react-storage-and-handler/src/components/utilities/reduce.js
@@ -1,6 +1,14 @@
-const numbers = [20, 30, 40, 50]
-const sumReducer = (previous, current) => previous + current
-const total = numbers.reduce(sumReducer, 0)
-return total;
+// const numbers = [20, 30, 40, 50]
+// const sumReducer = (previous, current) => previous + current
+// const total = numbers.reduce(sumReducer, 0)
+// return total;
 
-export {sumReducer}
+
+
+const items = [
+    {id:1, name:"Jibon"},
+    {id:2, name:"Zibon"},
+    {id:2, name:"Payel"},
+]
+const itemReducer = (previous, current) => previous + current;
+const itemTotal = items.reduce(itemReducer, 0)
